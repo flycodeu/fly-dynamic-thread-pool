a20abd4d2b13a570a1048c6cacf8e94b35bb911f
flycodeu <1785613048@qq.com>
2025-07-18 09:19:54 +0800

    feat(添加动态线程池Redis数据库连接):

--- fly-dynamic-thread-pool-springboot-stater/src/main/java/com/flycode/ThreadPool/config/DynamicThreadPoolConfig.java
+++ fly-dynamic-thread-pool-springboot-stater/src/main/java/com/flycode/ThreadPool/config/DynamicThreadPoolConfig.java
@@ -1,7 +1,12 @@
 package com.flycode.ThreadPool.config;
 
 import com.alibaba.fastjson.JSON;
+import com.flycode.ThreadPool.entity.DynamicThreadPoolRedisEntity;
 import com.flycode.ThreadPool.service.DynamicThreadPoolService;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -29,5 +34,26 @@ public class DynamicThreadPoolConfig {
         }
         logger.info("当前应用:{}, threadPoolInfo:{}", applicationName, JSON.toJSONString(threadPoolExecutorMap));
         return new DynamicThreadPoolService(applicationName, threadPoolExecutorMap);
+    }
+
+
+    @Bean("redisClient")
+    public RedissonClient redissonClient(DynamicThreadPoolRedisEntity dynamicThreadPoolRedisEntity) {
+        Config config = new Config();
+        config.setCodec(JsonJacksonCodec.INSTANCE);
+        config.useSingleServer()
+                .setAddress(String.format("redis://%s:%s", dynamicThreadPoolRedisEntity.getHost(), dynamicThreadPoolRedisEntity.getPort()))
+                .setDatabase(dynamicThreadPoolRedisEntity.getDatabase())
+                .setKeepAlive(dynamicThreadPoolRedisEntity.isKeepAlive())
+                .setRetryAttempts(dynamicThreadPoolRedisEntity.getRetryAttempts())
+                .setPingConnectionInterval(dynamicThreadPoolRedisEntity.getPingInterval())
+                .setConnectTimeout(dynamicThreadPoolRedisEntity.getConnectTimeout())
+                .setPassword(dynamicThreadPoolRedisEntity.getPassword())
+                .setConnectionMinimumIdleSize(dynamicThreadPoolRedisEntity.getMinIdleSize())
+        ;
+
+        RedissonClient redissonClient = Redisson.create(config);
+        logger.info("动态线程池注册中心Redis Host: {},端口：{}启动成功", dynamicThreadPoolRedisEntity.getHost(), dynamicThreadPoolRedisEntity.getPort());
+        return redissonClient;
     }
 }
